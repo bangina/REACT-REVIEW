0f7b5e6f2b20a99bd34042303a6c2e39bea080a7
ina <bbangina@gmail.com>
2021-01-04 19:14:14 +0900

    refactor:seperate loading state

--- redux-middleware/src/containers/SampleContainer.jsx
+++ redux-middleware/src/containers/SampleContainer.jsx
@@ -14,8 +14,17 @@ const SampleContainer = ({
   loadingUsers,
 }) => {
   useEffect(() => {
-    getPost(1);
-    getUsers(1);
+    //useEffect에 바로 async 붙일 수 없으므로
+    //임시로 내부에 함수 선언해서 async붙여서 호출해서 사용함
+    const fn = async () => {
+      try {
+        await getPost(1);
+        await getUsers(1);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fn();
   }, [getPost, getUsers]);
   //get 함수들 : 요청해서 결과까지 payload까지 가져오는 async await 함수
   return (
@@ -30,11 +39,11 @@ const SampleContainer = ({
 
 //리덕스와 connect
 export default connect(
-  ({ sample }) => ({
+  ({ sample, loading }) => ({
     post: sample.post,
     users: sample.users,
-    loadingPost: sample.loading.GET_POST,
-    loadingUsers: sample.loading.GET_USERS,
+    loadingPost: loading["sample/GET_POST"],
+    loadingUsers: loading["sample/GET_USERS"],
   }),
   {
     getPost,
